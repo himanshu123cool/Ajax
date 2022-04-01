12672bd47441ed562a0940c05e65b6cec8efe73a
himanshu123cool <97112194+himanshu123cool@users.noreply.github.com>
2022-04-01 07:31:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const btn = document.getElementById('btn');
-const content = document.getElementById('content');
 
-btn.addEventListener('click', getData);
-
-function getData(){
-    url = 'file.json'
-    fetch(url).then((response)=>{
-        return response.json()
-    }).then((data)=>{
-        content.innerHTML = JSON.stringify(data.employee1.salary);
-        console.log(data);
-    })
-}
-
-getData();
